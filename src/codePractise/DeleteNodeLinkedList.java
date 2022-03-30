3f9d68703aae396f9e56bcc2f4267d716eb9a9b3
chiragsp49 <85429959+chiragsp49@users.noreply.github.com>
2022-03-30 09:56:18 +0530

    Completed Lowest Common Ancestor problem

---
+++ src/codePractise/DeleteNodeLinkedList.java
@@ -0,0 +1,6 @@
+package codePractise;
+
+//https://leetcode.com/problems/delete-node-in-a-linked-list/
+public class DeleteNodeLinkedList {
+
+}
--- src/codePractise/LowestCommonAncBST.java
+++ src/codePractise/LowestCommonAncBST.java
@@ -1,6 +1,27 @@
 package codePractise;
 
 //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+//https://www.youtube.com/watch?v=6POfA8fruxI
 public class LowestCommonAncBST {
+	
+	 //Definition for a binary tree node.
+	 public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	  }
+	 
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if(p.val<root.val && q.val<root.val) {
+			return lowestCommonAncestor(root.left, p, q);
+		}
+		
+		if(p.val>root.val && q.val>root.val) {
+			return lowestCommonAncestor(root.right, p, q);
+		}
+		
+		return root;
+    }
 
 }
